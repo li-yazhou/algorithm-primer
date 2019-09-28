ba1716166522c1d5f278c876c6e10d91ea699eab
liyazhou <yazhou6543@gmail.com>
2019-09-28 10:52:40 +0800

    docs: 34 在排序数组中查找元素的第一个和最后一个位置

---
+++ leetcode/src/main/java/leetcode/_0034_FirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,51 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * @desc    在排序数组中查找元素的第一个和最后一个位置
+ * @author  liyazhou1
+ * @date    2019/9/28
+ */
+
+/**
+ * <pre>
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ *
+ *
+ * 示例 1:
+ *    输入: nums = [5,7,7,8,8,10], target = 8
+ *    输出: [3,4]
+ *
+ * 示例 2:
+ *    输入: nums = [5,7,7,8,8,10], target = 6
+ *    输出: [-1,-1]
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </pre>
+ */
+public class _0034_FirstAndLastPositionOfElementInSortedArray {
+
+    class Solution {
+
+        public int[] searchRange(int[] nums, int target) {
+
+            return null;
+        }
+
+
+        @Test
+        public void test() {
+
+        }
+    }
+
+}
