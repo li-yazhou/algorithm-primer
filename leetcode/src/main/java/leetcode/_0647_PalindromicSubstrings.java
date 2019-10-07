e8b0a86bf4b819d39d645b3d734f17bfe7e4480f
liyazhou <yazhou6543@gmail.com>
2019-10-07 22:34:33 +0800

    source: 647, Medium, Palindromic Substrings 回文子串

---
+++ leetcode/src/main/java/leetcode/_0647_PalindromicSubstrings.java
@@ -0,0 +1,90 @@
+package leetcode;
+
+/**
+ * @No          647
+ * @problem     Palindromic Substrings
+ * @level       Medium
+ * @desc        回文子串
+ * @author      liyazhou1
+ * @date        2019/10/07
+ *
+ * <pre>
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ *
+ * The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+ *
+ * Example 1:
+ *
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ *
+ *
+ * Example 2:
+ *
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ *
+ *
+ * Note:
+ *
+ * The input string length won't exceed 1000.
+ * </pre>
+ */
+public class _0647_PalindromicSubstrings {
+
+
+    /**
+     * Note
+     *
+     * Thought
+     *      方法一，中心扩展法
+     *      方法二，动态规划
+     *
+     * Challenge
+     *
+     * Algorithm
+     *      1.
+     *      2.
+     *
+     * Complexity
+     *      Time,
+     *      Space,
+     */
+    private static class Solution {
+
+        /**
+         * 方法一，中心扩展法
+         */
+        public int countSubstrings(String s) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i ++) {
+                count += palindromicSubstrings(s, i, i);
+                count += palindromicSubstrings(s, i, i+1);
+            }
+            return count;
+        }
+
+
+        private int palindromicSubstrings(String str, int left, int right) {
+            int count = 0;
+            while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+                count ++;
+                left --;
+                right ++;
+            }
+            return count;
+        }
+
+        public static void main(String[] args) {
+            String[] inputs = {"abc", "aaa", "fdsklf"};
+            for (String input: inputs) {
+                System.out.println("input = " + input);
+                int result = new Solution().countSubstrings(input);
+                System.out.println("result = " + result);
+                System.out.println("-----------------");
+            }
+        }
+    }
+}
