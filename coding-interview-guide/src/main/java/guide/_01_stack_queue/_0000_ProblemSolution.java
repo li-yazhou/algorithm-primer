0affcf75718c42e31ed7b96417ea3017553ca9e4
liyazhou <yazhou6543@gmail.com>
2020-02-02 11:22:03 +0800

    update: 程序员代码面试指南

---
+++ coding-interview-guide/src/main/java/guide/_01_stack_queue/_0000_ProblemSolution.java
@@ -0,0 +1,30 @@
+package guide._01_stack_queue;
+
+/**
+ * @No
+ * @problem
+ * @tag
+ * @author      liyazhou1
+ * @date        2019/10/01
+ *
+ * <pre>
+ *
+ * </pre>
+ */
+public class _0000_ProblemSolution {
+
+
+    /**
+     * Note
+     *
+     * Thought
+     *
+     * Algorithm
+     *      1.
+     *      2.
+     *      3.
+     */
+    private static class Solution {
+
+    }
+}
---
+++ coding-interview-guide/src/main/java/guide/_04_recursion_dynamic_program/_0000_ProblemSolution.java
@@ -0,0 +1,30 @@
+package guide._04_recursion_dynamic_program;
+
+/**
+ * @No
+ * @problem
+ * @tag
+ * @author      liyazhou1
+ * @date        2019/10/01
+ *
+ * <pre>
+ *
+ * </pre>
+ */
+public class _0000_ProblemSolution {
+
+
+    /**
+     * Note
+     *
+     * Thought
+     *
+     * Algorithm
+     *      1.
+     *      2.
+     *      3.
+     */
+    private static class Solution {
+
+    }
+}
