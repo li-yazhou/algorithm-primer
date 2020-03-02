11388269fb8f210a062793e1e76c71ba25611550
liyazhou <yazhou6543@gmail.com>
2020-03-02 22:06:54 +0800

    update: leetcode 416 等值分割问题，使用递归方法实现排列组合，由此改变题目【求数组所有子集的和】

--- leetcode/src/main/java/leetcode/_0416_PartitionEqualSubsetSum.java
+++ leetcode/src/main/java/leetcode/_0416_PartitionEqualSubsetSum.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @No          416
@@ -29,6 +29,144 @@ import java.util.Arrays;
  * </pre>
  */
 public class _0416_PartitionEqualSubsetSum {
+
+
+    /**
+     * Note
+     *  改编题目，求数组任意子集的元素和，即每个元素不参与运算或者仅参与一次运算
+     *
+     * Thought
+     *
+     * Challenge
+     *
+     * Algorithm
+     *      1.
+     *      2.
+     *      3.
+     *
+     * Complexity
+     *      Time,
+     *      Space,
+     */
+    private static class Solution2 {
+
+        Set<Integer> result = new HashSet<>();
+
+        public Set<Integer> subsums(int[] nums) {
+            subsums(nums, 0, nums.length-1);
+            return result;
+        }
+
+        private void subsums(int[] nums, int sum, int i) {
+            if (i < 0) {
+                return;
+            }
+
+            int taken = sum + nums[i];
+            int nottaken = sum;
+            result.add(taken);
+            result.add(nottaken);
+
+            subsums(nums, taken, i-1);
+            subsums(nums, nottaken, i-1);
+
+        }
+
+
+        public static void main(String[] args) {
+            int[][] inputs = {
+                    {1, 5, 11, 5},
+                    {1, 2, 3, 5},
+            };
+            for (int[] input: inputs) {
+                System.out.println("2 input = " + Arrays.toString(input));
+                Set<Integer> result = new Solution2().subsums(input);
+                List<Integer> list = new ArrayList<>(result);
+                Collections.sort(list);
+                System.out.println("1 list = " + list);
+                System.out.println("---------------");
+            }
+        }
+    }
+
+
+    /**
+     * Note
+     *
+     * Thought
+     *
+     * Challenge
+     *
+     * Algorithm
+     *      1.
+     *      2.
+     *      3.
+     *
+     * Complexity
+     *      Time,
+     *      Space,
+     */
+    private static class Solution1 {
+
+        public boolean canPartition(int[] nums) {
+            if (nums == null || nums.length < 2) {
+                return false;
+            }
+
+            int sum = 0;
+            for (int num: nums) {
+                sum += num;
+            }
+
+            if ((sum & 1) == 1) {
+                return false;
+            }
+
+            int target = sum / 2;
+
+            return subsum(nums, target, nums.length-1);
+        }
+
+
+        private boolean subsum(int[] nums, int target, int i) {
+            if (i < 0) {
+                return  false;
+            }
+
+            int taken = target - nums[i];
+            int nottaken = target;
+
+            if (taken == 0 || nottaken == 0) {
+                return true;
+            }
+
+            return subsum(nums, taken, i-1) || subsum(nums, nottaken, i-1);
+        }
+
+
+        public static void main(String[] args) {
+            int[][] inputs = {
+                    {1, 5, 11, 5},
+                    {1, 2, 3, 5},
+            };
+            for (int[] input: inputs) {
+                System.out.println("1 input = " + Arrays.toString(input));
+                boolean result = new Solution1().canPartition(input);
+                System.out.println("1 result = " + result);
+                System.out.println("---------------");
+            }
+
+            int[] input = {1, 5, 11, 5};
+            int[] targets = {1, 5, 10, 11, 12, 16, 17, 21, 22, 23, 2, 6};
+            System.out.println("target = " + Arrays.toString(targets));
+            for (int target : targets) {
+                System.out.println("target = " + target);
+                boolean result = new Solution1().subsum(input, target, input.length-1);
+                System.out.println("result = " + result);
+                System.out.println("++++++++++++++++++");
+            }
+        }
+    }
 
 
     /**
